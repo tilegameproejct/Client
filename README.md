68fca5697d8bd7286852d2c9a6dca901a46cff43
YongsunLee <katarite715@naver.com>
2019-04-21 16:29:00 +0900

    참고중인 Map 생성기
    참고중인 Map 생성기

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
-# Client
+﻿# Client
 클라이언트 관련 저장소
+
+참고 Project
+Map Generation System
+-> https://youtu.be/vQgLdFNrCN8
---
+++ SystemTestProject/Assets/Scripts/MoveToPos.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveToPos : MonoBehaviour {
+    // 좌클릭시 선택될 Object
+    Transform SelectObject;
+    Transform TilePosition;
+
+    private bool Moving;
+
+    // Use this for initialization
+    void Start () {
+        Moving = false;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+
+        if (Moving && SelectObject != null)
+        {
+            MoveToTile(TilePosition);
+        }
+
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hitInfo;
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            if (Physics.Raycast(ray, out hitInfo))
+            {
+                SelectObject = hitInfo.transform;
+
+            }
+        }
+        else if (Input.GetMouseButtonDown(1)) {
+
+            if (Physics.Raycast(ray, out hitInfo))
+            {
+                TilePosition = hitInfo.transform;
+                if (SelectObject != null) Moving = true;
+
+                Debug.Log(hitInfo.transform.gameObject.name);
+            }
+        }
+
+        //Debug.Log(SelectObject.gameObject.name);
+    }
+
+    void MoveToTile(Transform tilePos)
+    {
+        if (SelectObject.position == tilePos.position) {
+            Moving = false;
+            SelectObject = null;
+        }
+        else
+        {
+            Vector3 dir = tilePos.position - SelectObject.position;
+            dir.y = 0f;
+            dir = Vector3.Normalize(dir);
+
+            SelectObject.forward = dir;
+            SelectObject.position += 2f * dir * Time.deltaTime;
+        }
+    }
+}
