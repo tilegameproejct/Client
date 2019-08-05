71da00b6de9e16863ac9aab745810d662d02ddf1
YongsunLee <katarite715@naver.com>
2019-08-05 18:55:43 +0900

    턴제 시스템 고민중..

---
+++ TurnGameSys/Assets/Script/Movement.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour {
+
+	public GameObject Prefab;
+
+	public GameObject RedCharacterPrefab;
+	public GameObject BlueCharacterPrefab;
+
+	GameObject Map;
+	Transform InitObject;
+
+	GameObject Selector;
+
+	int TurnCnt = 0;
+
+	// Use this for initialization
+	void Start () {
+		Map = GameObject.Find("Map");
+		InitObject = Map.transform.GetChild(0);
+		Prefab.transform.position = InitObject.position;
+
+		Selector = Instantiate(Prefab,Prefab.transform.position, Prefab.transform.rotation);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetKeyDown(KeyCode.LeftArrow) == true)
+		{	
+			Selector.transform.Translate(-3, 0 , 0);
+		}
+		if(Input.GetKeyDown(KeyCode.RightArrow) == true)
+		{
+			Selector.transform.Translate(3, 0 , 0);
+		}
+
+		if(Input.GetKeyDown(KeyCode.UpArrow) == true)
+		{
+			Selector.transform.Translate(0, 0 , 3);
+		}
+		if(Input.GetKeyDown(KeyCode.DownArrow) == true)
+		{
+			Selector.transform.Translate(0, 0 , -3);
+		}
+		
+		if(Input.GetKeyDown(KeyCode.LeftControl) == true)
+		{	
+			if(TurnCnt == 0)
+			{
+				Instantiate(RedCharacterPrefab, Selector.transform.position, RedCharacterPrefab.transform.rotation);
+				TurnCnt = 1;
+			}
+			else
+			{
+				Instantiate(BlueCharacterPrefab, Selector.transform.position, BlueCharacterPrefab.transform.rotation);
+				TurnCnt = 0;
+			}
+		}
+	}
+}
---
+++ TurnGameSys/Assets/Script/TurnSystem.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TurnSystem : MonoBehaviour {
+	public GameObject SelectPrefab;
+	GameObject RedTeam;
+	GameObject BlueTeam;
+
+	Transform[] RedTeamCharcter = new Transform[3];
+	Transform[] BlueTeamCharcter = new Transform[3];
+
+	// Use this for initialization
+	void Start () {
+		RedTeam = GameObject.Find("RedTeam");
+		BlueTeam = GameObject.Find("BlueTeam");
+
+		for(int i = 0; i < RedTeam.transform.childCount; i++){
+			RedTeamCharcter[i] = RedTeam.transform.GetChild(i);
+		}
+
+		for(int i = 0; i < BlueTeam.transform.childCount; i++){
+			BlueTeamCharcter [i] = BlueTeam.transform.GetChild(i);
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
